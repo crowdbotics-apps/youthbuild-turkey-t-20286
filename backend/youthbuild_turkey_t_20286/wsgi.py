87cb3238eb09747b4d1dd1a55d93f1ddd56daa22
Crowdbotics <team@crowdbotics.com>
2020-09-15 15:14:13 +0000

    Initial commit.

---
+++ backend/youthbuild_turkey_t_20286/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for youthbuild_turkey_t_20286 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youthbuild_turkey_t_20286.settings')
+
+application = get_wsgi_application()
